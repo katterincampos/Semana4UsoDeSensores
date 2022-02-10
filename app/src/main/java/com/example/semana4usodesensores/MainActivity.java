f515b90e67793d0ac351d01260a4080cbcd81594
ErickTiznado <88002820+ErickTiznado@users.noreply.github.com>
2022-02-10 17:37:14 -0600

    Solucion de errores

--- app/src/main/java/com/example/semana4usodesensores/MainActivity.java
+++ app/src/main/java/com/example/semana4usodesensores/MainActivity.java
@@ -1,48 +1,47 @@
 package com.example.semana4usodesensores;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
 import android.os.Bundle;
-import com.example.semana4usodesensores.Controlador.PagerController;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
-    ViewPager viewPager;
-    TabItem tab1,tab2,tab3,tab4;
 
-    PagerController pagerAdapter;
+    TabItem tab1,tab2,tab3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tabLayout = findViewById(R.id.tablayout);
+        tabLayout = findViewById(R.id.menu);
 
-        viewPager = findViewById(R.id.viewuno);
         tab1 = findViewById(R.id.Home);
         tab2 = findViewById(R.id.SensorProxi);
         tab3 = findViewById(R.id.SensorAcelerometro);
 
-        pagerAdapter = new PagerController(getSupportFragmentManager(),tabLayout.getTabCount());
-        viewPager.setAdapter(pagerAdapter);
+
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
 
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 // Obtenemos la posición del tab a mostrar
-                viewPager.setCurrentItem(tab.getPosition());
                 // Determinar cual fragment se presenta
                 if(tab.getPosition()==0){
-                    pagerAdapter.notifyDataSetChanged();
+                    Intent siga = new Intent(MainActivity.this,MainActivity.class);
+                    startActivity(siga);
                 }
                 if(tab.getPosition()==1){
-                    pagerAdapter.notifyDataSetChanged();
+                    Intent siga = new Intent(MainActivity.this,SensorProximidad.class);
+                    startActivity(siga);
                 }
                 if(tab.getPosition()==2){
-                    pagerAdapter.notifyDataSetChanged();
+                    Intent siga = new Intent(MainActivity.this,Ascelerometro.class);
+                    startActivity(siga);
                 }
 
 
@@ -57,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
     }
 
 
